1b5072f74768f8c611242182a4567b05167fe50b
Dylan-haiji <yanghaij327@163.com>
2019-12-05 10:37:15 +0800

    init zuul and update consumer / zipkin

--- javayh-nacos-consumer/src/main/java/com/javayh/nacos/consumer/NacosConsumerApplication.java
+++ javayh-nacos-consumer/src/main/java/com/javayh/nacos/consumer/NacosConsumerApplication.java
@@ -2,15 +2,14 @@ package com.javayh.nacos.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class NacosComsumerApplication {
+public class NacosConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NacosComsumerApplication.class, args);
+        SpringApplication.run(NacosConsumerApplication.class, args);
     }
 
 }
---
+++ javayh-nacos-zuul/src/main/java/com/javayh/nacos/zuul/ZuulApplication.java
@@ -0,0 +1,20 @@
+package com.javayh.nacos.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Description zuul
+ * @author Dylan
+ * @date 2019/12/5
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+}
