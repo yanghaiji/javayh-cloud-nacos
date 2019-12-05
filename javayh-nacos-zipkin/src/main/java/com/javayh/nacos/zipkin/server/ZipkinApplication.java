aaab67bb405b0d9854084b0ef464f6c166ae13b3
Dylan-haiji <yanghaij327@163.com>
2019-12-05 18:05:55 +0800

    add zipkin

--- javayh-nacos-zipkin/src/main/java/com/javayh/nacos/zipkin/server/ZipkinApplication.java
+++ javayh-nacos-zipkin/src/main/java/com/javayh/nacos/zipkin/server/ZipkinApplication.java
@@ -2,7 +2,12 @@ package com.javayh.nacos.zipkin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import zipkin.server.internal.EnableZipkinServer;
+import zipkin2.storage.mysql.v1.MySQLStorage;
+
+import javax.sql.DataSource;
+
 
 @EnableZipkinServer//开启ZipkinServe
 @SpringBootApplication
@@ -12,4 +17,8 @@ public class ZipkinApplication {
         SpringApplication.run(ZipkinApplication.class, args);
     }
 
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.newBuilder().datasource(datasource).executor(Runnable::run).build();
+    }
 }
