dfc2565f9b2cd7a6ee2d968d1c17f68b254da9a6
Dylan-haiji <yanghaij327@163.com>
2019-12-23 11:43:17 +0800

    未验证,估计有bug

---
+++ javayh-nacos-tc/src/main/java/com/javayh/nacos/tc/TcApplication.java
@@ -0,0 +1,17 @@
+package com.javayh.nacos.consumer;
+
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableDistributedTransaction
+@EnableFeignClients
+@SpringBootApplication
+public class TcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcApplication.class, args);
+    }
+
+}
