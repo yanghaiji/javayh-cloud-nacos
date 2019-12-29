5566e0df68c902d669215d59dbc4015575943908
Dylan-haiji <yanghaij327@163.com>
2019-12-29 21:22:02 +0800

    修改依赖问题

--- javayh-nacos-consumer/src/main/java/com/javayh/nacos/consumer/NacosConsumerApplication.java
+++ javayh-nacos-consumer/src/main/java/com/javayh/nacos/consumer/NacosConsumerApplication.java
@@ -1,11 +1,9 @@
 package com.javayh.nacos.consumer;
 
-import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDistributedTransaction
 @EnableFeignClients
 @SpringBootApplication
 public class NacosConsumerApplication {
